b395355ac22e24d9b4f1339767a2404f7d473df7
Ben <benjaminctyner@gmail.com>
2023-07-05 09:11:44 -0400

    port

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -11,7 +11,6 @@ import { CallToAction } from '@/components/CallToAction'
 import { Footer } from '@/components/Footer'
 import projImage from '@/images/aiIconGen.png'
 import github from '@/images/github.png'
-// import projImage from '@/images/stock/gallery-07.jpg'
 
 const projects = [
   {
@@ -23,19 +22,27 @@ const projects = [
     source_code_link: 'https://github.com/benjaminctyner/icon-generator-ai/',
     host_link: 'http://icons.ben-t.dev',
   },
+  // {
+  //   name: 'AI Icon Generator2',
+  //   description:
+  //     'Web-based, AI powered, icon generator that saves you from paying for a designer and wasting time.',
+
+  //   image: projImage,
+  //   source_code_link: 'https://github.com/benjaminctyner/icon-generator-ai/',
+  //   host_link: 'http://icons.ben-t.dev',
+  // },
 ]
 
 const ProjectCard = ({
   name,
   description,
-
   image,
   source_code_link,
   host_link,
 }) => {
   return (
-    <div className="flex h-full w-full items-center justify-center">
-      <div className="bg-tertiary w-full rounded-2xl p-4 sm:w-[360px]">
+    <div className="flex h-full w-full items-center justify-center pb-[50px]">
+      <div className=" w-full rounded-2xl bg-gray-100 p-4 sm:w-[360px]">
         <div className="relative h-[230px] w-full">
           <Image
             src={image}
@@ -58,13 +65,13 @@ const ProjectCard = ({
         </div>
 
         <div
-          className="mt-0 cursor-pointer"
+          className="-mt-[20px] cursor-pointer"
           onClick={() => window.open(host_link, '_blank')}
         >
-          <h3 className="text-[24px] font-bold text-white hover:underline">
+          <h3 className="text-[24px] font-bold text-slate-900 hover:underline">
             {name}
           </h3>
-          <p className="text-secondary text-[14px]">{description}</p>
+          <p className="text-[14px] text-slate-900 ">{description}</p>
         </div>
       </div>
     </div>
@@ -84,7 +91,7 @@ export default function Home() {
       </Head>
 
       <Header />
-      <div className="mt-20 flex flex-wrap gap-7 bg-vanilla">
+      <div className="mt-[50px] flex flex-wrap gap-7 bg-vanilla">
         {projects.map((project, index) => (
           <ProjectCard key={`project-${index}`} index={index} {...project} />
         ))}
