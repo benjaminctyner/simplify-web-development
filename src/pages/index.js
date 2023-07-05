5428363e26469755faaa83ed83e13a3ed4d4af84
Ben <benjaminctyner@gmail.com>
2023-07-05 09:46:54 -0400

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,7 +29,7 @@ export default function Home() {
       <FeatureBlocks />
       <FeaturesGrid />
       <Process />
-      <TestimonialsSlide />
+      {/* <TestimonialsSlide /> */}
 
       <CallToAction />
       <Footer />
